92ddd130f98f300507fd971b4a817a4dc80b8db6
Nicolas PICON <nicopico.dev@gmail.com>
2016-09-27 22:03:53 +0200

    App: loading contact

--- app/src/main/java/fr/nicopico/happybirthday/App.kt
+++ app/src/main/java/fr/nicopico/happybirthday/App.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import fr.nicopico.happybirthday.inject.AppComponent
 import fr.nicopico.happybirthday.inject.AppModule
 import fr.nicopico.happybirthday.inject.DaggerAppComponent
+import timber.log.Timber
 
 class App : Application() {
 
@@ -13,6 +14,10 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         component = DaggerAppComponent.builder().appModule(AppModule(this)).build()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
 }
--- app/src/main/java/fr/nicopico/happybirthday/ui/home/MainActivity.kt
+++ app/src/main/java/fr/nicopico/happybirthday/ui/home/MainActivity.kt
@@ -2,18 +2,24 @@ package fr.nicopico.happybirthday.ui.home
 
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import fr.nicopico.happybirthday.R
 import fr.nicopico.happybirthday.data.repository.Repository
 import fr.nicopico.happybirthday.domain.model.Contact
 import fr.nicopico.happybirthday.inject.AppComponent
 import fr.nicopico.happybirthday.ui.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import rx.android.schedulers.AndroidSchedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 class MainActivity : BaseActivity() {
 
     @Inject
     lateinit var contactRepository: Repository<Contact>
+    private var subscription: rx.Subscription? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,12 +31,49 @@ class MainActivity : BaseActivity() {
             adapter = contactAdapter
         }
 
-        contactRepository.list().toList().subscribe {
-            contactAdapter.data = it
+        loadContacts()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_reload) {
+            loadContacts()
+            return true
         }
+        else {
+            return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroy() {
+        subscription?.unsubscribe()
+        super.onDestroy()
     }
 
     override fun inject(component: AppComponent) {
         component.inject(this)
+    }
+
+    private fun loadContacts() {
+        subscription = contactRepository.list()
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .toList()
+                .subscribe(object : rx.Observer<List<Contact>> {
+                    override fun onNext(t: List<Contact>) {
+                        (rvContacts.adapter as ContactAdapter).data = t
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Timber.e(e, "Unable to retrieve contact")
+                    }
+
+                    override fun onCompleted() {
+                        Toast.makeText(this@MainActivity, "OK", Toast.LENGTH_SHORT).show()
+                    }
+                })
     }
 }
